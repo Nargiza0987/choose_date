28586b0ef877ea756698cb0a08931cdc36ab4c28
Nargiza0987 <kulm1987@gmail.com>
2023-11-06 10:01:45 +0300

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1935),
+      lastDate: DateTime(2025),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[300],
+      body: Center(
+        child: MaterialButton(
+          onPressed: _showDatePicker,
+          child: const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Choose Date',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          color: Colors.cyan[200],
+        ),
+      ),
+    );
+  }
+}
